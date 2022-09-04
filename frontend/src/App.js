e818283f3b76a3dd767a8e11601482fbc8e4d52e
markfoster314 <markfoster314@yahoo.com>
2022-09-03 20:54:13 -0600

    Getting Set Up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,53 @@
-import logo from './logo.png';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mark Foster's Personal Website
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/markfoster314/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/markfoster314"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-      </header>
+    <div className="main-wrapper home-two">
+      <div id="home-banner" style={{ backgroundColor: "#2d3038" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-9">
+              <div className="banner-content">
+                <div className="home-social">
+                  <ul className="list-inline">
+                    <li>
+                      <a
+                        style={{ fontSize: "24px", color: "white" }}
+                        href="https://www.facebook.com/markfoster314"
+                      >
+                        <i className="fa fa-facebook" aria-hidden="true"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        style={{ fontSize: "24px", color: "white" }}
+                        href="https://github.com/markfoster314"
+                      >
+                        <i className="fa fa-github" aria-hidden="true"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        style={{ fontSize: "24px", color: "white" }}
+                        href="https://www.linkedin.com/in/markfoster314/"
+                      >
+                        <i className="fa fa-linkedin" aria-hidden="true"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="banner-info">
+                  <h1 style={{ color: "white" }}>my name is</h1>
+                  <h2 style={{ color: "white" }}>Mark Foster</h2>
+                  <h3 style={{ color: "white" }}>
+                    explorer. thinker. creator.
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "./css/animate.css"
+import "./css/bootstrap.min.css"
+import "./fonts/fontAwesome/font-awesome.min.css"
+import "./css/main.css"
+import "./css/magnific-popup.css"
+import "./css/responsive.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
