8b1c065780c8af1f669a4545650324b8b8d516a5
markfoster314 <markfoster314@yahoo.com>
2022-09-03 19:02:53 -0600

    Basic Website

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Mark Foster's Personal Website
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/markfoster314/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/markfoster314"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
